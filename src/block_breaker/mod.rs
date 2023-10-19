62022fc9178fa13ca3db35fb04e0dd5286c8eaac
Isaac <icorbrey@gmail.com>
2023-10-18 23:00:40 -0400

    feat: Add wall components

--- src/block_breaker/mod.rs
+++ src/block_breaker/mod.rs
@@ -1,13 +1,14 @@
+mod camera;
 mod tileset;
 mod wall;
 
 use bevy::prelude::*;
 
-use self::wall::WallPlugin;
+use self::{camera::CameraPlugin, wall::WallPlugin};
 
 pub fn run() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_plugins(WallPlugin)
+        .add_plugins((CameraPlugin, WallPlugin))
         .run()
 }
--- src/block_breaker/wall.rs
+++ src/block_breaker/wall.rs
@@ -6,18 +6,68 @@ pub struct WallPlugin;
 
 impl Plugin for WallPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, WallAsset::load);
+        app.add_systems(Startup, WallAsset::load)
+            .add_systems(Update, spawn_walls);
     }
+}
+
+fn spawn_walls(mut commands: Commands, asset: Res<WallAsset>) {
+    commands.spawn_batch(
+        (0..10)
+            .map(|x| {
+                asset.get_sprite(
+                    WallTile::Horizontal,
+                    Transform::from_translation(Vec3::new(160.0 - 32.0 * x as f32, 0.0, 0.0)),
+                )
+            })
+            .collect::<Vec<SpriteSheetBundle>>(),
+    );
 }
 
 #[derive(Resource)]
 pub struct WallAsset(pub Handle<TextureAtlas>);
 
+pub enum WallTile {
+    Vertical,
+    Horizontal,
+    TopLeft,
+    TopRight,
+    BottomRight,
+    BottomLeft,
+}
+
+impl Into<usize> for WallTile {
+    fn into(self) -> usize {
+        match self {
+            Self::Vertical => 0,
+            Self::Horizontal => 1,
+            Self::TopLeft => 2,
+            Self::TopRight => 3,
+            Self::BottomRight => 4,
+            Self::BottomLeft => 5,
+        }
+    }
+}
+
+impl WallAsset {
+    pub fn get_sprite(&self, variant: WallTile, transform: Transform) -> SpriteSheetBundle {
+        SpriteSheetBundle {
+            texture_atlas: self.0.clone(),
+            transform,
+            sprite: TextureAtlasSprite {
+                index: variant.into(),
+                ..default()
+            },
+            ..default()
+        }
+    }
+}
+
 impl Tileset for WallAsset {
     const PATH: &'static str = "tilesets/block-breaker-wall.png";
-    const TILE_SIZE: bevy::prelude::Vec2 = Vec2::splat(16.0);
+    const TILE_SIZE: bevy::prelude::Vec2 = Vec2::splat(32.0);
     const COLUMNS: usize = 3;
-    const ROWS: usize = 3;
+    const ROWS: usize = 2;
 
     fn new(tileset: Handle<TextureAtlas>) -> Self {
         Self(tileset)
